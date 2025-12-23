3ce3e77f7e9d1669c960cdf280ce248c4268125c
vishalicious213 <vishalicious213@gmail.com>
2025-12-22 20:08:14 -0800

    imported & rendered Header, Main & Footer in App

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,13 @@
-<script setup></script>
+<script setup>
+  import Header from '@/components/Header.vue'
+  import Main from '@/components/Main.vue'
+  import Footer from '@/components/Footer.vue'
+</script>
 
-<template></template>
+<template>
+  <Header />
+  <Main />
+  <Footer />
+</template>
 
 <style scoped></style>
